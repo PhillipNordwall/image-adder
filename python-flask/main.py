6964ccc3d2f0f41c4ed1ceb68df864ea2489c2d3
Phillip Nordwall <Phillip.Nordwall+github@gmail.com>
2020-10-31 18:22:37 -0700

    Fix page bug and add default path

--- python-flask/main.py
+++ python-flask/main.py
@@ -1,16 +1,21 @@
+import re
 import flask
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/
 import requests
 # https://requests.readthedocs.io/en/master/user/quickstart/
 
 app = flask.Flask(__name__)
+REG = "[bB]ee"
+REP = "buzz-buzz<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Vespa_Mandarinia_Magnifica_-_Filippo_Turetta.jpg/220px-Vespa_Mandarinia_Magnifica_-_Filippo_Turetta.jpg'>"
 
-@app.route('/', defaults={'path': ''})
+
+@app.route('/', defaults={'path': 'en.wikipedia.org/wiki/bee'})
 @app.route('/<path:path>')
 def proxy(path):
   page = requests.get(f'https://{path}').content.decode("UTF-8")
   # page can be manipulated here as a "UTF-8" string
-  return page
+  newpage = re.sub(REG, REP, page)
+  return newpage
   
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080, ssl_context='adhoc')
