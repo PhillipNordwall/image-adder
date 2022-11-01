0c27f129bc0cef50b10ca9893e3fba09b5f9a3f5
Phillip Nordwall <Phillip.Nordwall@gmail.com>
2022-10-31 17:12:46 -0700

    initial soda song

---
+++ soda/soda_song.py
@@ -0,0 +1,6 @@
+"""Very naive bottles of soda on the wall"""
+
+print("1 bottles of soda on the wall")
+print("1 bottles of soda")
+print("take one down and pass it around")
+print("no bottles of soda on the wall")
